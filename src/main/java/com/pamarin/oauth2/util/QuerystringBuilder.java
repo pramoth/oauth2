ea0c24b677a516d87fb3a992498be626fc7003c5
jittagornp <jittagornp@gmail.com>
2017-10-22 16:52:59 +0700

    add QuerystringBuilder

---
+++ src/main/java/com/pamarin/oauth2/util/QuerystringBuilder.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.oauth2.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author jittagornp <http://jittagornp.me>
+ * create : 2017/10/22
+ */
+public class QuerystringBuilder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QuerystringBuilder.class);
+
+    private Map<String, Object> params;
+
+    private Map<String, Object> getParams() {
+        if (params == null) {
+            params = new LinkedHashMap<>();
+        }
+        return params;
+    }
+
+    public QuerystringBuilder addParameter(String name, Object value) {
+        getParams().put(name, value);
+        return this;
+    }
+
+    private String encode(String url) {
+        try {
+            return URLEncoder.encode(url, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException ex) {
+            LOG.warn("Unsupported encoding", ex);
+            return null;
+        }
+    }
+
+    public String build() {
+        return getParams().entrySet()
+                .stream()
+                .map(param -> {
+                    return param.getKey() + "=" + encode(String.valueOf(param.getValue()));
+                }).collect(Collectors.joining("&"));
+    }
+}
---
+++ src/test/java/com/pamarin/oauth2/util/QuerystringBuilderTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.oauth2.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+/**
+ * @author jittagornp <http://jittagornp.me>
+ * create : 2017/10/22
+ */
+public class QuerystringBuilderTest {
+
+    @Test
+    public void shouldBeEmptyString_whenEmptyParameter() {
+        QuerystringBuilder input = new QuerystringBuilder();
+        String output = input.build();
+        String expected = "";
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeResponseTypeIsCode() {
+        QuerystringBuilder input = new QuerystringBuilder()
+                .addParameter("response_type", "code");
+        String output = input.build();
+        String expected = "response_type=code";
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeResponseTypeIsCode_andScopeIsRead() {
+        QuerystringBuilder input = new QuerystringBuilder()
+                .addParameter("response_type", "code")
+                .addParameter("scope", "read");
+        String output = input.build();
+        String expected = "response_type=code&scope=read";
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeResponseTypeIsCode_andScopeIsRead_andRedirecUriIsEncodedHttpLocalhost() {
+        QuerystringBuilder input = new QuerystringBuilder()
+                .addParameter("response_type", "code")
+                .addParameter("scope", "read")
+                .addParameter("redirect_uri", "http://localhost/callback");
+        String output = input.build();
+        String expected = "response_type=code&scope=read&redirect_uri=http%3A%2F%2Flocalhost%2Fcallback";
+        assertThat(output).isEqualTo(expected);
+    }
+
+}
